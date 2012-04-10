b3d5b62e9fb42e23d4219cba5fbc2bae76c014b8
Shubham Gupta <shubham.gupta@vinsol.com>
2012-04-10 22:47:34 +0530

    first commit

--- ExamFinal.js
+++ ExamFinal.js
@@ -4,60 +4,65 @@ this.options=opts;
 this.ans=ans;
 }
 Quiz.startQuiz=function(){
-//Quiz.loadQuiz();
 Quiz.nextQuiz();
 }
 Quiz.loadQuiz=function(){
+var json_obj = [
+{"ques" :"Expand HTML" , "options":["HyperTrimMarkUpLang", "HiTextMarkUpLanguage", "HyperTextMarkUpLanguage", "HyperTextMarkUpLang"], "ans":2},
+{"ques" :"Expand XML" , "options":["HyperTrimMarkUpLang", "HiTextMarkUpLanguage", "ExtensibleMarkUpLanguage", "HyperTextMarkUpLang"], "ans":2},
+{"ques" :"Expand TCP" , "options":["No idea", "Transmission control protocol", "ExtensibleMarkUpLanguage", "HyperTextMarkUpLang"], "ans":1},
+{"ques" :"Expand FTP" , "options":["No idea", "Transmission control protocol", "File transmission Protocol", "HyperTextMarkUpLang"], "ans":2},
+{"ques" :"Expand UDP" , "options":["User Datagram Protocol", "Transmission control protocol", "File transmission Protocol", "HyperTextMarkUpLang"], "ans":0}];
 var quesArr=new Array();
-var q1_opt=["o1","o2","o3","o4"];
-var q1 = new Quiz("quest1", q1_opt, 1);
-var q2_opt=["oo1","oo2","oo3","oo4"];
-var q2 = new Quiz("quest2", q2_opt, 0);
-var q3_opt=["o1","o2","o3","o4"];
-var q3 = new Quiz("quest3", q3_opt, 3);
-var q4_opt=["o1","o2","o3","o4"];
-var q4 = new Quiz("quest4", q4_opt, 2);
-var q5_opt=["o1","o2","o3","o4"];
-var q5 = new Quiz("quest5", q5_opt, 4);
-quesArr[0]=q1;
-quesArr[1]=q2;
-quesArr[2]=q3;
-quesArr[3]=q4;
-quesArr[4]=q5;
+for(var i=0;i<json_obj.length;i++){
+quesArr[i]=new Quiz(json_obj[i].ques,json_obj[i].options,json_obj[i].ans);
+}
+var quesDiv= document.createElement("div");
+quesDiv.id="quesDiv";
+var quesTxt = document.createElement("label");
+quesTxt.id="quesTxt";
+var optDiv= document.createElement("div");
+optDiv.id="optDiv";
+quesDiv.appendChild(quesTxt);
+var ins=new Array();
+var optLabel=new Array();
+for(var i=0;i<4;i++){
+ins[i] = document.createElement("input");
+ins[i].type="radio";
+ins[i].name="rad";
+ins[i].className="radioBut";
+optLabel[i] = document.createElement("label");
+optLabel[i].innerHTML="<br/>";
+optDiv.appendChild(ins[i]);
+optDiv.appendChild(optLabel[i]);
+}
+var but = document.createElement("input");
+but.type="Button";
+but.id="proceedBtn";
+but.value="Proceed";
+quesDiv.appendChild(optDiv);
+quesDiv.appendChild(but);
+document.body.appendChild(quesDiv);
 return quesArr;
 }
 Quiz.nextQuiz=function(){
 Quiz.nextQuiz.count++;
-if(Quiz.nextQuiz.count<quesArr.length){
-quesDiv.innerHTML="";
-quesArr[Quiz.nextQuiz.count].show();
-}
-else{
-document.writeln("Quiz completed");
-}
+if(Quiz.nextQuiz.count<quesArr.length) quesArr[Quiz.nextQuiz.count].show();
+else document.writeln("Quiz completed");
 }//nextQuiz
 Quiz.nextQuiz.count=-1;
 Quiz.prototype={
 show: function(){
 var that = this;
-var txt = document.createTextNode("Quiz : "+this.ques);
-quesDiv.appendChild(txt);
-var opt_div = document.createElement("div");
-quesDiv.appendChild(opt_div);
-var optLabel = new Array();
-var ins = new Array();
+document.getElementById("quesTxt").innerHTML="Question : "+this.ques;
+var opt_div = document.getElementById("optDiv");
+var optLabel = opt_div.getElementsByTagName("label");
+var ins =document.getElementsByClassName("radioBut");
 for(var i=0;i<4;i++){
-ins[i] = document.createElement("input");
-ins[i].type="radio";
-ins[i].name="rad";
-optLabel[i] = document.createElement("label");
+ins[i].checked=false;
 optLabel[i].innerHTML= this.options[i]+"<br/>";
-opt_div.appendChild(ins[i]);
-opt_div.appendChild(optLabel[i]);
 }
-var but = document.createElement("input");
-but.type="button";
-but.value="Next";
+var but = document.getElementById("proceedBtn");
 var isChecked=false;
 but.onclick=function checkAnswer(){
 for(var j =0; j<ins.length; j++){
@@ -67,15 +72,11 @@ break;
 }
 }//for
 if(isChecked){
-if(that.ans==j){
-alert("Your Answer Is Correct");
-}
-else{
-alert("Your Answer Is InCorrect.");
-}
+if(that.ans==j) alert("Your Answer Is Correct");
+else alert("Your Answer Is InCorrect.");
 }//if isChecked
 Quiz.nextQuiz();
 }//checkAnswer ends
-quesDiv.appendChild(but);
 }//show
 }
+
